0b5546214571b0b86f20cead94c6723b652e2ee0
Denis Sakhno <97180503+ideniska@users.noreply.github.com>
2023-08-14 15:25:55 -0400

    add map data from api to table rows

---
+++ frontend/src/components/table/table.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import './table.css';
+
+function Table() {
+    const [tableData, setTableData] = useState([])
+    const fetchTableData = () => {
+      fetch(process.env.REACT_APP_GET_ORDERS_URL).then(response => {
+        return response.json()
+      }).then(data => {
+        setTableData(data)        
+      })
+      
+      
+    }
+    useEffect(() => {
+      fetchTableData()
+    }, [])
+
+    useEffect(() => {
+      console.log("tableData", tableData);
+    }, [tableData]);
+
+  return (
+    <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+                <th>Дата</th>
+                <th>Номер заказа</th>
+                <th>Состав заказа</th>
+                <th>Статус заказа</th>
+                <th>Статус оплаты</th>
+                <th>Сумма</th>
+                <th>ФИО</th>
+                <th>Chat_ID</th>
+                <th>Email</th>
+                <th>Телефон</th>
+                <th>Примечание</th>
+            </tr>
+          </thead>
+          <tbody>
+          {tableData.length > 0 && tableData.map(row => (
+                <tr key={row.order_id}>
+                  <td>{row.order_date}</td>
+                  <td>{row.order_id}</td>
+                  <td>{row.order_content.join(", ")}</td>
+                  <td>{row.order_status}</td>
+                  <td>{row.payment_status}</td>
+                  <td>{row.total_price}</td>
+                  <td>{`${row.client.first_name} ${row.client.second_name}`}</td>
+                  <td>{row.client.chat_id}</td>
+                  <td>{row.client.email}</td>
+                  <td>{row.client.phone}</td>
+                  <td>{row.order_comment}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+    </div>
+  );
+}
+
+export default Table;
