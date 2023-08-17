b3b604f0acf60e62a88c9f25a5f399abdf9ea779
Denis Sakhno <97180503+ideniska@users.noreply.github.com>
2023-08-17 09:55:50 -0400

    add 20000 records, new get orders view, pagination

--- pizza_crm/settings.py
+++ pizza_crm/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -124,3 +125,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
--- pizza_crm/urls.py
+++ pizza_crm/urls.py
@@ -5,4 +5,6 @@ from api.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("get_orders/", get_orders),
+    path("add_fake/", add_fake_data),
+    path("get_past_orders/", get_past_orders),
 ]
